465fcbe5006e259abc0482834cbfdf01d41bd21c
amlepeshkin <79627739+amlepeshkin@users.noreply.github.com>
2024-08-04 19:10:54 +0300

    Update alpha 0.3
    Added asteroids

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-#Update alpha 0.2
+#Update alpha 0.3
 import pygame as p
 from sprites import *
-import random
+import math
 p.init()
 playerFile = "G8_up.png"
 playerFile2 = "G8_down.png"
 playerFile3 = "G8_left.png"
 playerFile4 = "G8_right.png"
 mapFile = "Global_map(2).png"
+asteroidFile = "Asteroid.png"
 # fulscreen size = [1370, 700]
 size = [1320, 680]
 window = p.display.set_mode(size)
@@ -15,36 +16,49 @@ p.display.set_caption("Dangerous World")
 screen = p.Surface(size)
 f1 = p.font.Font(None, 36)
 f2 = p.font.Font(None, 100)
-speed = 1
+speed = 10
+a = 0
+b = 0
+asteroid_number = 25
 debug = False
 todgle_shack = True
 todgle_debug = True
+asteroids = []
 
 wmap = Worldmap(screen, mapFile, -2300, -1300, speed)
 player = Player(screen, playerFile, playerFile2, playerFile3, playerFile4)
 hbar = Bar(screen, 50, 600, 0, 100, 255, 0, 0)
 fbar = Bar(screen, 50, 640, 0, 10, 0, 255, 0)
+for _ in range(asteroid_number):
+    #a = random.randint(0, 22000)
+    #b = random.randint(0, 9000)
+    a, b = rand(wmap)
+    if (True):
+        asteroids.append(Asteroid(screen, asteroidFile, a, b))
 
 def draw():
-    #                          color       x     y    width   height
+    #                          color       x     y    width   height 
     #p.draw.rect(screen, (255, 255, 255), (0,    0,    200,    50))
     if (debug):
         p.draw.rect(screen, (255, 255, 255), (0, 0, 200, 50))
     p.draw.rect(screen, (150, 0, 150), (0, 580, 300, 100))
 
 def render_text(f1, text, show, x, y):
-    if (show):
+    if (show):  
         text1 = f1.render(str(text)  , 1, (0, 0, 0))
         screen.blit(text1, (x, y))
 
 def render_all():
     wmap.render()
+    for ast in asteroids:
+        ast.render()
     draw()
     player.render()
     hbar.render()
     fbar.render()
     render_text(f1, (wmap.x, wmap.y), debug, 10, 10)
-    render_text(f2, 'OUT OF FUEL!', wmap.out, 400, 100)
+    render_text(f2, 'OUT OF FUEL!', (fbar.range <= fbar.min), 400, 100)
+    render_text(f2, 'SPASESHIP IS BROKEN!', (hbar.range <= hbar.min), 300, 100)
 
 running = True    
 while (running):
@@ -69,8 +83,13 @@ while (running):
                     debug = False
                     todgle_debug = True
 
-    wmap.move(speed, fbar)
+    wmap.move(speed, fbar, hbar)
     player.move(fbar)
+    for ast in asteroids:
+        ast.move(wmap, fbar, hbar)
+        if (intersect(size[0]/2-player.width/2, size[1]/2-player.height/2, ast.x+ast.width/2, ast.y+ast.height/2, ast.height/2)):
+            hbar.damage(abs(ast.speed_x + ast.speed_y))
+            ast.delete(wmap)
     
     screen.fill([64, 0, 64])
     render_all()
--- sprites.py
+++ sprites.py
@@ -1,7 +1,23 @@
-#Update alpha 0.2
+#Update alpha 0.3
 import pygame as p
+import random
 p.init()
 size = [1320, 680]
+
+def intersect(s1_x, s1_y, s2_x, s2_y, range):
+    if ((s1_x>s2_x-range) and (s1_x<s2_x+range) and (s1_y>s2_y-range) and (s1_y<s2_y+range)):
+        return True
+    else:
+        return False
+    
+def rand(pl):
+    a = random.randint(0, 22000)
+    b = random.randint(0, 9000)
+    while (not(a < pl.x - size[0] or a > pl.x + size[0] and b < pl.y - size[1] or b > pl.y + size[1])):
+        a = random.randint(0, 22000)
+        b = random.randint(0, 9000)
+    return a, b
+
 
 
 class Bar():
@@ -31,11 +47,11 @@ class Worldmap ():
         self.out = False
     def render(self):
         self.screen.blit(self.bitmap,(self.x, self.y) )
-    def move(self, speed, bar):
+    def move(self, speed, bar, bar2):
         self.speed = speed
         change = 1000
         keyp = p.key.get_pressed()
-        if (bar.range > 0):
+        if (bar.range > 0 and bar2.range > 0):
             if (keyp[p.K_w]):
                 self.y += self.speed
                 bar.damage(speed/change)
@@ -48,7 +64,7 @@ class Worldmap ():
             if (keyp[p.K_d]):
                 self.x -= self.speed
                 bar.damage(speed/change)
-        else:
+        elif (bar.range <= 0 and not(bar2.range > 0)):
             self.out = True
 
 
@@ -76,3 +92,37 @@ class Player ():
         self.height, self.width = self.bitmap.get_height(), self.bitmap.get_width()
     def render(self):
         self.screen.blit(self.bitmap,(size[0]/2-self.width/2, size[1]/2-self.height/2))
+
+
+
+class Asteroid ():
+    def __init__(self, screen, bitmap, x, y):
+        self.screen = screen
+        self.bitmap = p.image.load(bitmap)
+        self.x, self.y = x, y
+        self.speed_x, self.speed_y = random.randint(-2, 2),  random.randint(-2, 2)
+        #self.speed_x, self.speed_y = 0,  0
+        self.height, self.width = self.bitmap.get_height(), self.bitmap.get_width()
+    def move(self, wmap, bar, bar2):
+        if (self.x > 25000 or self.x < -3000 or self.y > 12000 or self.y < -3000):
+            self.speed_x, self.speed_y = random.randint(-5, 5),  random.randint(-5, 5)
+            self.x = random.randint(0, 22000)
+            self.y = random.randint(0, 9000)
+        self.speed = wmap.speed
+        keyp = p.key.get_pressed()
+        if (bar.range > 0 and bar2.range > 0):
+            if (keyp[p.K_w]):
+                self.y += self.speed
+            if (keyp[p.K_a]):
+                self.x += self.speed
+            if (keyp[p.K_s]):
+                self.y -= self.speed
+            if (keyp[p.K_d]):
+                self.x -= self.speed
+        self.x += self.speed_x
+        self.y += self.speed_y
+    def delete(self, player):
+        self.speed_x, self.speed_y = random.randint(-5, 5),  random.randint(-5, 5)
+        self.x, self.y = rand(player)
+    def render(self):
+        self.screen.blit(self.bitmap,(self.x, self.y) )
